947a4a4afb194e531202e2ed06c9b2ba0228e550
I Komang Ariana <komang.ariana@dana.id>
2020-05-21 17:34:17 +0800

    run function on DOMContentLoad to get city data from storage and get the forecast data for each of it.

--- scripts/app.js
+++ scripts/app.js
@@ -178,9 +178,27 @@
     });
   };
 
-  app.updateForecastCard(injectedForecast);
-
   app.saveSelectedCities = function () {
     window.localforage.setItem('selectedCities', app.selectedCities);
   };
+
+  document.addEventListener('DOMContentLoaded', function() {
+    window.localforage.getItem('selectedCities', function (err, cityList) {
+      if (cityList) {
+        app.selectedCities = cityList;
+        app.selectedCities.forEach(function (city) {
+          app.getForecast(city.key, city.label);
+        });
+      } else {
+        app.updateForecastCard(injectedForecast);
+        app.selectedCities = [
+          {
+            key: injectedForecast.key,
+            label: injectedForecast.label
+          }
+        ];
+        app.saveSelectedCities();
+      }
+    })
+  })
 })();
